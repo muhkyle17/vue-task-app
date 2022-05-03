5bd32fff5d8fc3342d4582644d8d7ba9220fd33e
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-05-03 16:09:04 -0700

    Add delete task functionality

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
   },
   data() {
     return {
       tasks: [],
     }
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
   },
   created() {
     this.tasks = [
@@ -23,7 +33,7 @@ export default {
         id: 1,
         text: 'Doctors Appointment',
         day: 'March 1st at 2:30pm',
-        remainder: true,
+        reminder: true,
       },
       {
         id: '2',
