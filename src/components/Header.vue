7a1b129c112a8da3f01f28e41d385949bdaa02ab
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-05-03 23:13:02 -0700

    Add conditional styling to rendering the color of the Add Task button to Close button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
-      @toggle-add-task="$emit('toggle-add-task')"
-      text="Add Task"
-      color="green"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'Red' : 'Green'"
     />
   </header>
 </template>
@@ -16,6 +16,7 @@ export default {
   name: 'Header',
   props: {
     title: String,
+    showAddTask: Boolean,
   },
   components: {
     Button,
