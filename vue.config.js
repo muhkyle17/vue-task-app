d29fa74c87d9824d18387868f6b2e4f958c9ef52
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-05-04 09:50:14 -0700

    Add API to app config for api responses to the backend

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/API': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/API': '/' },
+      },
+    },
+  },
 })
